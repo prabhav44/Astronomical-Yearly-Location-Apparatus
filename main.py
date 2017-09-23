fc1cecb199e8da594c431f3b30dffc4bb6a8efa3
prabhav44 <prabhav.parashar@gmail.com>
2017-09-22 21:37:40 -0400

    main python file

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # so we will be adding code that is ready for deployment here
 
+#So we have successful extraction of ephemeris data from JPL
+
 import callhorizons
 import numpy as np
+import datetime
+
+IoMoon = callhorizons.query(501, smallbody=False)
+
+IoMoon.set_discreteepochs([2458010])
+
+IoMoon.get_ephemerides('500@599')
+
+print(IoMoon['RA'], IoMoon['DEC'])
