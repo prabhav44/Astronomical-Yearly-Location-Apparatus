5c2b107c70f89792106f10277b601ddd3837890f
Prabhav Parashar <prabhav.parashar@gmail.com>
2017-10-02 18:18:43 -0400

    formatting

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 import callhorizons
-
-
-# define horizons function that outputs Ephem data for specified planet and target body
-# pass orbiting body, julian day, and target body ID and get RA, DEC at specified instance
-
-def horizon(orbit_body, julian_day, target_body_string):
-    data = callhorizons.query(orbit_body, smallbody=False)
-    data.set_discreteepochs([julian_day])
-    data.get_ephemerides(target_body_string)
-    return float(data['RA']), float(data['DEC'])
-
-    # create loop that feeds in 48 hours of import callhorizons
 from ctypes import *
 import datetime
 
@@ -39,6 +27,3 @@ class JDHorizon:
         data.set_discreteepochs([julian_day_n])
         data.get_ephemerides(target_body_string)
         return float(data['RA']), float(data['DEC'])
-Julian days to get Ephem data at each instance
-    # store ephem data into list or SQLite DB
-    # split 48 hours into intervals dependent on PC speed (future)
