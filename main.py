d05ca60641d09e30910bef8bd6e93dfa22003f02
Prabhav Parashar <prabhav.parashar@gmail.com>
2017-10-13 19:45:34 -0400

    Update main.py
    Multicore Support and foundations of DB_generation

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import callhorizons
-from ctypes import *
-from datetime import *
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from math import floor
+from numba.decorators import generated_jit, jit
 
 
 class JDHorizonData:
     @staticmethod
+    @jit
     def jd_alg(year, month, day, hour, minute, second):
-        # make sure dll file is in same directory as main.py
+        ysincebc = year + 4800 - floor((14 - month) / 12)
+        msincebc = month + 12 * floor((14 - month) / 12) - 3
 
-        jd_alg_c = cdll.LoadLibrary('JDalg_final.dll')
-        # loading dll file above
-        # setting argtypes for C function
-        jd_alg_c.JulianDay.argtypes = [c_int, c_int, c_int, c_int]
-        # defining c function result as a python variable
-        julian_day = jd_alg_c.JulianDay(year, month, day, hour)
-        # adding hour minute second interval decimal places because of C floating point errors
-        julian_day_n = julian_day + ((hour - 12) / 24) \
-                                  + (minute / 1440) \
-                                  + (second / 86400)
-        return julian_day_n
+        jdn = day + floor((153 * msincebc + 2) / 5) + (365 * ysincebc) + floor(ysincebc / 4) - floor(
+            ysincebc / 100) + floor(ysincebc / 400) - 32045
+
+        if 0 <= hour < 12:
+            jdn += 1
+
+        jdn = jdn + ((hour - 12) / 24) \
+              + (minute / 1440) \
+              + (second / 86400)
+
+        return jdn
 
     @staticmethod
     def horizon_data(orbit_body, target_body, start_d, end_d, step):
@@ -28,41 +31,44 @@ class JDHorizonData:
         data.get_ephemerides(target_body)
         return data['RA'], data['DEC']
 
-    # run when may 24th + defined year is true, defined year changes everytime it's run
+    gd_first = '2017-05-24 01:00'
+    gd_last = '2018-05-24 01:00'
+
     @staticmethod
-    def db_gen():
-        # global first bound variable in UTC, in future have year updated when it downloads new .db file at this date
-        # year later
-        gd_first = '2017-05-24 03:00'
-        gd_list = [datetime.strptime(gd_first, '%Y-%m-%d %H:%M')]
+    def db_gen_gd(gd_first):
+        # separating function for performance
+        # cannot multicore datetime process
+        def time_add(time, minute_counter):
+            new_time = time + relativedelta(minutes=minute_counter)
+            return new_time
+        # done so easy to change starting date
+        gd_list = []
+        first = datetime.strptime(gd_first, '%Y-%m-%d %H:%M')
+        # generating primary gd_list which is divied into 10 min increments to feed into horizons
+        for i in range(0, 525601, 1):
+            new = time_add(first, i)
+            gd_list.append(str(new))
+        return gd_list
 
-        # adding a years worth of minutes to time variable then converting to str so we can feed to callhorizons
-        for i in range(1, 525602, 1):
-            gd_list.append(str(datetime.strptime(gd_first, '%Y-%m-%d %H:%M') + relativedelta(minutes=i)))
+    # this algorithm should only be as slow as data pull
+    @staticmethod
+    def db_gen_ra_dec(gd_first, gd_last):
+        # actual not interpolated data use this to get alg for interpolating data, run this parallel with gd_gen
+        real_ra, real_dec = JDHorizonData.horizon_data(501, '500@599', gd_first, gd_last, '10 m')
 
-        # since first variable is untouched when adding timedelta to time variables and converting them to strings
-        gd_list[0] = str(gd_list[0])
+        @jit
+        def ra_i(real_ra):
+            ra = [None] * 525601
+            j = 0
+            for i in real_ra:
+                ra[j] = i
+                j += 10
+            return ra
 
-        # master list is gd_list which will be fed into sqlite insert command
+        ra =  ra_i(real_ra)
 
-        # chunks of data to feed horizons since it only supports 90024 lines a time
-        gd_hz_chnk1 = gd_list[0:87600]
-        gd_hz_chnk2 = gd_list[87600:175200]
-        gd_hz_chnk3 = gd_list[175200:262800]
-        gd_hz_chnk4 = gd_list[262800:350400]
-        gd_hz_chnk5 = gd_list[350400:438000]
-        gd_hz_chnk6 = gd_list[438000:-1]
+        def ra_inter(ra):
+            while type(None) in ra:
+                brad_alg(ra)
+            return ra
 
-        # this class and function however is essentially the core of our backend which can be housed on a central server
-        # see how this will be time intensive though? because it relies on network speed not processor
-        # in progress implement radians call to JPL, function finished just need to integrate within master
-        
-        # below lines are temporary ugly lines of code, will optimize 10/9/2017, need to concatenate all RA, DEC data
-        # into single list to be fed into DB
-        # don't judge fuckers
-        RA_chnk1, DEC_chnk1 = JDHorizonData.horizon_data(501, '500@599', gd_hz_chnk1[0], gd_hz_chnk1[-1], '1m')
-        RA_chnk2, DEC_chnk2 = JDHorizonData.horizon_data(501, '500@599', gd_hz_chnk2[0], gd_hz_chnk2[-1], '1m')
-        RA_chnk3, DEC_chnk3 = JDHorizonData.horizon_data(501, '500@599', gd_hz_chnk3[0], gd_hz_chnk3[-1], '1m')
-        RA_chnk4, DEC_chnk4 = JDHorizonData.horizon_data(501, '500@599', gd_hz_chnk4[0], gd_hz_chnk4[-1], '1m')
-        RA_chnk5, DEC_chnk5 = JDHorizonData.horizon_data(501, '500@599', gd_hz_chnk5[0], gd_hz_chnk5[-1], '1m')
-        RA_chnk6, DEC_chnk6 = JDHorizonData.horizon_data(501, '500@599', gd_hz_chnk6[0], gd_hz_chnk6[-1], '1m')
