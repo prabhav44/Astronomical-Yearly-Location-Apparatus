5619b0736597e822c82a60bd79e1ca75b50c54d7
Prabhav Parashar <prabhav.parashar@gmail.com>
2017-10-14 15:54:19 -0400

    Update brad_alg.py

--- brad_alg.py
+++ brad_alg.py
@@ -18,7 +18,26 @@ def brad_alg(ra):
                     ra[j-6] = ((ra[j-5] - ra[j-7]) / 2) + ra[j-7]
 
                     # now define values after halfway point
-    return ra
+                    ra[j-3] = ((ra[j] - ra[j-5]) / 2) + ra[j-5]
+                    ra[j-4] = ((ra[j-3] - ra[j-5]) / 2) + ra[j-5]
+                    ra[j-2] = ((ra[j] - ra[j-3]) / 2) + ra[j-3]
+                    ra[j-1] = ((ra[j] - ra[j-2]) / 2) + ra[j-2]
+
+                    # Beautiful
                 # else if lower bound is greater then alternate
-                #elif ra[j] < ra[j-i]:
-                    # execute alt alg
+                elif ra[j] < ra[j-i]:
+                    # in future going to have to split these intervals into smaller sub ones to account for even numbers
+                    # defining before halfway point
+                    ra[j-5] = ((360 - ra[j-i]) / 2) + ra[j-i]
+                    ra[j-8] = ((ra[j-5] - ra[j-i]) / 2) + ra[j-i]
+                    ra[j-9] = ((ra[j-8] - ra[j-i]) / 2) + ra[j-i]
+                    ra[j-7] = ((ra[j-5] - ra[j-8]) / 2) + ra[j-8]
+                    ra[j-6] = ((ra[j-5] - ra[j-7]) / 2) + ra[j-7]
+
+                    # now for after halfway point
+                    ra[j-4] = (ra[j] / 2)
+                    ra[j-2] = ((ra[j] - ra[j-4]) / 2) + ra[j-4]
+                    ra[j-3] = ((ra[j-2] - ra[j-4]) / 2) + ra[j-4]
+                    ra[j-1] = ((ra[j] - ra[j-2]) / 2) + ra[j-2]
+    return ra
+
