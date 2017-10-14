2dca92912a3aab5f17fdd418d5a56fdb035cc9d9
Prabhav Parashar <prabhav.parashar@gmail.com>
2017-10-14 19:56:19 -0400

    Boolin

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ class JDHorizonData:
         # actual not interpolated data use this to get alg for interpolating data, run this parallel with gd_gen
         real_ra, real_dec = JDHorizonData.horizon_data(501, '500@599', gd_first, gd_last, '10 m')
 
+        # these will run sequentially together concurrently with dec interpolation
         @jit
         def ra_i(real_ra):
             ra = [None] * 525601
@@ -65,10 +66,44 @@ class JDHorizonData:
                 j += 10
             return ra
 
-        ra =  ra_i(real_ra)
+        ra = ra_i(real_ra)
 
-        def ra_inter(ra):
-            while type(None) in ra:
-                brad_alg(ra)
+        @jit
+        def brad_alg(ra):
+            # analyze every block of 10
+            for i in range(10, len(ra), 10):
+                # in every block of 10 analyze every single one
+                for j in range(i - 10, i + 1, 1):
+                    if j == i:
+                        if ra[j] >= ra[j - 10]:
+                            ra[j - 5] = ((ra[j] - ra[j - 10]) / 2) + ra[j - 10]
+                            # now define values before halfway point
+                            ra[j - 8] = ((ra[j - 5] - ra[j - 10]) / 2) + ra[j - 10]
+                            ra[j - 9] = ((ra[j - 8] - ra[j - 10]) / 2) + ra[j - 10]
+                            ra[j - 7] = ((ra[j - 5] - ra[j - 8]) / 2) + ra[j - 8]
+                            ra[j - 6] = ((ra[j - 5] - ra[j - 7]) / 2) + ra[j - 7]
+
+                            # now define values after halfway point
+                            ra[j - 3] = ((ra[j] - ra[j - 5]) / 2) + ra[j - 5]
+                            ra[j - 4] = ((ra[j - 3] - ra[j - 5]) / 2) + ra[j - 5]
+                            ra[j - 2] = ((ra[j] - ra[j - 3]) / 2) + ra[j - 3]
+                            ra[j - 1] = ((ra[j] - ra[j - 2]) / 2) + ra[j - 2]
+
+                        elif ra[j] <= ra[j - 10]:
+                            # in future going to have to split these intervals into smaller sub ones to account for even numbers
+                            # defining before halfway point
+                            ra[j - 5] = ((360 - ra[j - 10]) / 2) + ra[j - 10]
+                            ra[j - 8] = ((ra[j - 5] - ra[j - 10]) / 2) + ra[j - 10]
+                            ra[j - 9] = ((ra[j - 8] - ra[j - 10]) / 2) + ra[j - 10]
+                            ra[j - 7] = ((ra[j - 5] - ra[j - 8]) / 2) + ra[j - 8]
+                            ra[j - 6] = ((ra[j - 5] - ra[j - 7]) / 2) + ra[j - 7]
+
+                            # now for after halfway point
+                            ra[j - 4] = (ra[j] / 2)
+                            ra[j - 2] = ((ra[j] - ra[j - 4]) / 2) + ra[j - 4]
+                            ra[j - 3] = ((ra[j - 2] - ra[j - 4]) / 2) + ra[j - 4]
+                            ra[j - 1] = ((ra[j] - ra[j - 2]) / 2) + ra[j - 2]
             return ra
+
+        ra = brad_alg(ra)
 
