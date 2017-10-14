8fc5250d921a6238a92b5d0782f54d7c2019cc9a
Prabhav Parashar <prabhav.parashar@gmail.com>
2017-10-14 19:52:45 -0400

    Made it faster

--- brad_alg.py
+++ brad_alg.py
@@ -1,43 +1,41 @@
 # gotta say, goddamn I'm good - Prabhav
 
+from numba.decorators import jit
+
+
+@jit
 def brad_alg(ra):
     # analyze every block of 10
     for i in range(10, len(ra), 10):
         # in every block of 10 analyze every single one
-        for j in range(i-10, i+1, 1):
-            # if j is equal to the upper bound
+        for j in range(i - 10, i + 1, 1):
             if j == i:
-                # if upper bound in RA is greater then run alg
-                if ra[j] > ra[j-i]:
-                    # middle bound between upper and lower bound
-                    ra[j-5] = ((ra[j] - ra[j-i]) / 2) + ra[j-i]
+                if ra[j] >= ra[j - 10]:
+                    ra[j - 5] = ((ra[j] - ra[j - 10]) / 2) + ra[j - 10]
                     # now define values before halfway point
-                    ra[j-8] = ((ra[j-5] - ra[j-i]) / 2) + ra[j-i]
-                    ra[j-9] = ((ra[j-8] - ra[j-i]) / 2) + ra[j-i]
-                    ra[j-7] = ((ra[j-5] - ra[j-8]) / 2) + ra[j-8]
-                    ra[j-6] = ((ra[j-5] - ra[j-7]) / 2) + ra[j-7]
+                    ra[j - 8] = ((ra[j - 5] - ra[j - 10]) / 2) + ra[j - 10]
+                    ra[j - 9] = ((ra[j - 8] - ra[j - 10]) / 2) + ra[j - 10]
+                    ra[j - 7] = ((ra[j - 5] - ra[j - 8]) / 2) + ra[j - 8]
+                    ra[j - 6] = ((ra[j - 5] - ra[j - 7]) / 2) + ra[j - 7]
 
                     # now define values after halfway point
-                    ra[j-3] = ((ra[j] - ra[j-5]) / 2) + ra[j-5]
-                    ra[j-4] = ((ra[j-3] - ra[j-5]) / 2) + ra[j-5]
-                    ra[j-2] = ((ra[j] - ra[j-3]) / 2) + ra[j-3]
-                    ra[j-1] = ((ra[j] - ra[j-2]) / 2) + ra[j-2]
+                    ra[j - 3] = ((ra[j] - ra[j - 5]) / 2) + ra[j - 5]
+                    ra[j - 4] = ((ra[j - 3] - ra[j - 5]) / 2) + ra[j - 5]
+                    ra[j - 2] = ((ra[j] - ra[j - 3]) / 2) + ra[j - 3]
+                    ra[j - 1] = ((ra[j] - ra[j - 2]) / 2) + ra[j - 2]
 
-                    # Beautiful
-                # else if lower bound is greater then alternate
-                elif ra[j] < ra[j-i]:
+                elif ra[j] <= ra[j - 10]:
                     # in future going to have to split these intervals into smaller sub ones to account for even numbers
                     # defining before halfway point
-                    ra[j-5] = ((360 - ra[j-i]) / 2) + ra[j-i]
-                    ra[j-8] = ((ra[j-5] - ra[j-i]) / 2) + ra[j-i]
-                    ra[j-9] = ((ra[j-8] - ra[j-i]) / 2) + ra[j-i]
-                    ra[j-7] = ((ra[j-5] - ra[j-8]) / 2) + ra[j-8]
-                    ra[j-6] = ((ra[j-5] - ra[j-7]) / 2) + ra[j-7]
+                    ra[j - 5] = ((360 - ra[j - 10]) / 2) + ra[j - 10]
+                    ra[j - 8] = ((ra[j - 5] - ra[j - 10]) / 2) + ra[j - 10]
+                    ra[j - 9] = ((ra[j - 8] - ra[j - 10]) / 2) + ra[j - 10]
+                    ra[j - 7] = ((ra[j - 5] - ra[j - 8]) / 2) + ra[j - 8]
+                    ra[j - 6] = ((ra[j - 5] - ra[j - 7]) / 2) + ra[j - 7]
 
                     # now for after halfway point
-                    ra[j-4] = (ra[j] / 2)
-                    ra[j-2] = ((ra[j] - ra[j-4]) / 2) + ra[j-4]
-                    ra[j-3] = ((ra[j-2] - ra[j-4]) / 2) + ra[j-4]
-                    ra[j-1] = ((ra[j] - ra[j-2]) / 2) + ra[j-2]
+                    ra[j - 4] = (ra[j] / 2)
+                    ra[j - 2] = ((ra[j] - ra[j - 4]) / 2) + ra[j - 4]
+                    ra[j - 3] = ((ra[j - 2] - ra[j - 4]) / 2) + ra[j - 4]
+                    ra[j - 1] = ((ra[j] - ra[j - 2]) / 2) + ra[j - 2]
     return ra
-
